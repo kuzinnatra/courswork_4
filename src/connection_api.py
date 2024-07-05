6da76f3bbf7b3a8ff4c6f2699078b575926dd5d9
kuzin_sn <kuzinnatra@yandex.ru>
2024-07-05 16:58:45 +0400

    Добавлен метод фильтрующий вакансии

--- src/connection_api.py
+++ src/connection_api.py
@@ -9,10 +9,10 @@ class AbstractApi(ABC):
     @abstractmethod
     def get_vacancies(self, text, per_page):
         pass
-    #
-    # @abstractmethod
-    # def get_filter_vacancies(self, text, per_page):
-    #     pass
+
+    @abstractmethod
+    def get_filter_vacancies(self, text, per_page):
+        pass
 
 
 class HH(AbstractApi):
@@ -34,6 +34,20 @@ class HH(AbstractApi):
         params = {'text': self.text, 'page': 0, 'per_page': self.per_page}
         response = requests.get(self.__url, params)
         return response.json()['items']
+
+
+    def get_filter_vacancies(self, per_page: int, text: str = ''):
+        filtered_vacancies = []
+        vacancies = self.get_vacancies(per_page, text)
+        for vacancy in vacancies:
+            filtered_vacancies.append({
+                "name": vacancy["name"],
+                "salary": vacancy["salary"],
+                "url": vacancy["alternate_url"],
+                "employer": vacancy["employer"]["name"]
+            })
+        return filtered_vacancies
+
 
 
 
