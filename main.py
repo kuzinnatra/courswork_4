898848746ef949550870faf102dd2c9569a3d142
kuzin_sn <kuzinnatra@yandex.ru>
2024-07-08 14:41:37 +0400

    добавлена функция сортировки вакансий по з/п файл main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.json_class import GetJson
 
 
 get_hh = HH()
-jsonfiletest = GetJson()
+my_vacancy = GetJson()
 quantity_vacancies = input("введите желаемое количество отображаемых вакансий от 1 до 100\n")
 name_vacancies = input("введите ключевые слова для поиска (например название профессии)\n")
 if int(quantity_vacancies) > 100:
@@ -13,9 +13,16 @@ if get_hh.get_vacancies(int(quantity_vacancies), name_vacancies) == []:
     print('Желаемые вакансии не найдены')
 else:
     vac = get_hh.get_vacancies(int(quantity_vacancies), name_vacancies)
-    jsonfiletest.write_data(vac)
-    vacanc = jsonfiletest.get_vacancies()
+    my_vacancy.write_data(vac)
+    vacanc = my_vacancy.get_vacancies()
     for i in vacanc:
         print(f'{i}\n')
-    jsonfiletest.delete_vacancy()
 
+    top_vac = int(input('Сколько вакансий показать с самой большой зарплатой?\nВведите число\n'))
+
+    vacanc.sort(key=lambda vacanc: vacanc.salary_from, reverse=True) #сортируем список вакансий по з/п
+
+    for i in range(0, top_vac):
+        print(f'{vacanc[i]}\n')
+
+    my_vacancy.delete_vacancy() #очищаем файл vacancies.json
