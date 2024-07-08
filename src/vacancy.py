9f4f0d84b1528084929359469ab370e2a5f16b14
kuzin_sn <kuzinnatra@yandex.ru>
2024-07-08 10:34:01 +0400

    добавлен перевод валюты в рубли vacancy.py

--- src/vacancy.py
+++ src/vacancy.py
@@ -13,10 +13,24 @@ class Vacancy:
         else:
             self.salary_from = salary["from"] if salary["from"] else 0
             self.salary_to = salary["to"] if salary["to"] else 0
+            #перевод иностранной валюты в рубли
+            if salary["currency"] == "USD":
+                self.salary_from = self.salary_from * 100
+                self.salary_to = self.salary_to * 100
+            if salary["currency"] == "UZS":
+                self.salary_from = int(self.salary_from * 0.007)
+                self.salary_to = int(self.salary_to * 0.007)
+            if salary["currency"] == "BYR":
+                self.salary_from = int(self.salary_from * 27)
+                self.salary_to = int(self.salary_to * 27)
+            if salary["currency"] == "KZT":
+                self.salary_from = int(self.salary_from * 0.2)
+                self.salary_to = int(self.salary_to * 0.2)
+
 
     def __str__(self):
-        return f"""Имя: {self.name}
-Зарплата от {self.salary_from} до {self.salary_to}
+        return f"""Профессия: {self.name}
+Зарплата от {self.salary_from} руб. до {self.salary_to} руб.
 url: {self.url}
 Название компании: {self.employer}"""
 
