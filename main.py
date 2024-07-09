65a6d998a42beef2377694e4df9641ff5082f2d4
kuzin_sn <kuzinnatra@yandex.ru>
2024-07-09 11:58:11 +0400

    Число отображаемых вакансий 100 (автоматически)

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ from src.json_class import GetJson
 
 get_hh = HH()
 my_vacancy = GetJson()
-quantity_vacancies = input("Введите желаемое количество отображаемых вакансий от 1 до 100:\n")
-name_vacancies = input("Введите ключевые слова для поиска (например название профессии):\n")
-if int(quantity_vacancies) > 100:
-    print("Превышено количество отображаемых вакансий (максимум 100)")
-    exit()
-if get_hh.get_vacancies(int(quantity_vacancies), name_vacancies) == []:
+name_vacancies = input("""Здравствуйте!
+Введите ключевые слова для поиска вакансий на сайте hh.ru (например название профессии):\n""")
+if get_hh.get_vacancies(100, name_vacancies) == []:
     print('Желаемые вакансии не найдены')
 else:
-    vac = get_hh.get_vacancies(int(quantity_vacancies), name_vacancies)
+    vac = get_hh.get_vacancies(100, name_vacancies)
     my_vacancy.write_data(vac)
     vacanc = my_vacancy.get_vacancies()
     for i in vacanc:
