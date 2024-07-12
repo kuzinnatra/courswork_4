2e2142aa97cf626c3c6f16f846ccdbaa69475baf
kuzin_sn <kuzinnatra@yandex.ru>
2024-07-12 16:53:20 +0400

    Вместо прямого получения вакансий в методе get_vacancies стал вызываться метод get_response, чтобы избежать дублирования кода.

--- src/connection_api.py
+++ src/connection_api.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 
 class AbstractApi(ABC):
     @abstractmethod
-    def get_response(self, url):
+    def get_response(self, per_page: int, text: str = ''):
         pass
     @abstractmethod
     def get_vacancies(self, per_page, text):
@@ -20,13 +20,15 @@ class HH(AbstractApi):
     def get_url(self):
         return self.__url
 
-    def get_response(self):
-        response = requests.get(self.__url)
+    def get_response(self, per_page: int, text: str = ''):
+        self.per_page = per_page
+        self.text = text
+        params = {'text': self.text, 'page': 0, 'per_page': self.per_page}
+        response = requests.get(self.__url, params)
         return response
 
     def get_vacancies(self, per_page: int, text: str = ''):
         self.per_page = per_page
         self.text = text
-        params = {'text': self.text, 'page': 0, 'per_page': self.per_page}
-        response = requests.get(self.__url, params)
+        response = self.get_response(self.per_page, self.text)
         return response.json()['items']
